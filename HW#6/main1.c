4a11b75eb2ef006ae357a5dd8f6b95bb41c4398a
B1129009 <120005700+B1129009@users.noreply.github.com>
2022-12-28 01:42:29 +0800

    Update main1.c

--- HW#6/main1.c
+++ HW#6/main1.c
@@ -3,10 +3,14 @@
 int main()
 {
     int i =10;
-    if(i%2==0){
-        printf("even\n");
+    if(i%15==0){
+        printf("Love IU\n");
+    }else if(i%5==0){
+        printf("IU\n");
+    }else if(i%3==0){
+        printf("Love\n");
     }else{
-        printf("odd\n");
+        printf("%d",i);
     }
     return 0;
 }
